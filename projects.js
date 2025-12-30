45addcc0a0b55e80fb8e817d6bbb51489bd24087
Pochodie <daniel@stagno.net>
2025-12-31 00:24:32 +0100

    Implemented function to generate project cards.

--- projects.js
+++ projects.js
@@ -5,7 +5,7 @@ const projects = [
         description: "My personal portfolio built with HTML, CSS, and JavaScript",
         category: "Web Development",
         technologies: ["HTML", "CSS", "JavaScript"],
-        image: "web.jpg",
+        image: "assets/images/projects/snooker.jpg",
         link: "https://example.com"
     },
     {
@@ -23,7 +23,7 @@ const projects = [
         description: "Practice scales",
         category: "Music",
         technologies: ["notation", "chords"],
-        image: "guitar.jpg",
+        image: "assets/images/projects/snooker.jpg",
         link: "https://example.com"
     },
     {
@@ -32,7 +32,7 @@ const projects = [
         description: "Make a dynamic website",
         category: "School Projects",
         technologies: ["HTML", "CSS", "JavaScript"],
-        image: "guitar.jpg",
+        image: "assets/images/projects/snooker.jpg",
         link: "https://example.com"
     },
     {
@@ -41,7 +41,7 @@ const projects = [
         description: "Video game",
         category: "Music",
         technologies: ["HTML", "CSS", "JavaScript"],
-        image: "guitar.jpg",
+        image: "assets/images/projects/snooker.jpg",
         link: "https://example.com"
     },
     {
@@ -50,10 +50,50 @@ const projects = [
         description: "Composing a piano concerto.",
         category: "Music",
         technologies: ["Notation", "Dorico", "Reaper"],
-        image: "guitar.jpg",
+        image: "assets/images/projects/snooker.jpg",
         link: "https://example.com"
     }
 
 
 ];
 
+function generateCards(category) {
+
+    const mainContainer = document.getElementById('project-cards-container');
+    mainContainer.replaceChildren();
+    let numberOfCards = 0;
+
+    projects.forEach(element => {
+
+
+        if (element.category === category || category === 'all') {
+
+            const projectDiv = document.createElement('div');
+            projectDiv.className = 'project-card';
+            const h3 = document.createElement('h3');
+            const description = document.createElement('p');
+            const image = document.createElement('img');
+            const labelCategory = document.createElement('label');
+            const link = document.createElement('a');
+            link.href = '#';
+            link.textContent = element.link;
+            image.src = element.image;
+            image.style = 'width: 50%; object-fit: contain';
+            labelCategory.textContent = element.category;
+
+            description.textContent = element.description;
+            h3.textContent = element.title;
+
+            projectDiv.appendChild(image);
+            projectDiv.appendChild(h3);
+            projectDiv.appendChild(description);
+            projectDiv.appendChild(labelCategory);
+            projectDiv.appendChild(link);
+            mainContainer.appendChild(projectDiv);
+            numberOfCards++;
+        }
+    });
+
+}
+
+generateCards('all');
