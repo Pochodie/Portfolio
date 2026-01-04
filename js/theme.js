8e06accf6eaf51d7b5317b27b2c467a1aaa86727
Daniel Stagno <daniel@stagno.net>
2026-01-04 13:35:50 +0100

    implemented transitions between light and dark background and text.

--- js/theme.js
+++ js/theme.js
@@ -1,4 +1,6 @@
 const themeButtons = document.querySelectorAll('.theme-switcher button');
+const darkBackground = document.getElementById('dark-background');
+const lightBackground = document.getElementById('light-background');
 
 themeButtons.forEach(button => {
     button.addEventListener('click', (event) => clickButton(event.target));
@@ -7,5 +9,18 @@ themeButtons.forEach(button => {
 
 
 function clickButton(button) {
-    console.log(button.getAttribute('data-theme'));
+    const selectedTheme = button.getAttribute('data-theme');
+    switch (selectedTheme) {
+        case 'light-theme':
+            document.body.classList.add('light-theme');
+            darkBackground.classList.remove('visible');
+            lightBackground.classList.add('visible');
+            console.log(document.body.classList);
+            break;
+        case 'dark-theme':
+            document.body.classList.remove('light-theme');
+            lightBackground.classList.remove('visible');
+            darkBackground.classList.add('visible');
+            break;
+    }
 }
