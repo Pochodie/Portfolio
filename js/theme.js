50c5725d036f9c37c7b5f34b25099ebaca6908a0
Daniel Stagno <daniel@stagno.net>
2026-01-04 13:53:11 +0100

    Added light version of header image.

--- js/theme.js
+++ js/theme.js
@@ -1,6 +1,9 @@
+const headerImage = document.getElementById('header-image');
 const themeButtons = document.querySelectorAll('.theme-switcher button');
+
 const darkBackground = document.getElementById('dark-background');
 const lightBackground = document.getElementById('light-background');
+
 
 themeButtons.forEach(button => {
     button.addEventListener('click', (event) => clickButton(event.target));
@@ -10,17 +13,31 @@ themeButtons.forEach(button => {
 
 function clickButton(button) {
     const selectedTheme = button.getAttribute('data-theme');
+    const imgElement = document.createElement('img');
+    headerImage.replaceChildren();
+    headerImage.classList.remove('visible');
+
     switch (selectedTheme) {
         case 'light-theme':
             document.body.classList.add('light-theme');
             darkBackground.classList.remove('visible');
             lightBackground.classList.add('visible');
-            console.log(document.body.classList);
+
+            imgElement.src = 'assets/images/me_light.png';
+            imgElement.alt = 'Photo of Daniel Stagno';
+            headerImage.appendChild(imgElement);
+            headerImage.classList.add('visible');
+
             break;
         case 'dark-theme':
             document.body.classList.remove('light-theme');
             lightBackground.classList.remove('visible');
             darkBackground.classList.add('visible');
+
+            imgElement.src = 'assets/images/me.png';
+            imgElement.alt = 'Photo of Daniel Stagno';
+            headerImage.appendChild(imgElement);
+            headerImage.classList.add('visible');
             break;
     }
 }
